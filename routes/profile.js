f225bbcc12eae4221d75c35489b1e36ea4af9e0d
Reece Poulsen <dreecepoulsen@gmail.com>
2022-11-23 15:35:22 -0700

    formatting and jukebox playlist integration

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,26 @@
-import { Router } from 'express';
-import isAuth from '../middleware/is-auth.js';
-import refSpotify from '../middleware/refSpotify.js';
-import { getMyProfile, getUser, getAllUserLites } from '../controllers/profile.js';
+import { Router } from "express";
+import isAuth from "../middleware/is-auth.js";
+import refSpotify from "../middleware/refSpotify.js";
+import {
+  getMyProfile,
+  getUser,
+  getAllUserLites,
+  getSongsInPlaylist,
+  updateWidgetPrivacy,
+  addSong,
+} from "../controllers/profile.js";
 
 const router = Router();
 
 router.get("/", isAuth, refSpotify, getMyProfile);
 
 router.get("/users", isAuth, getAllUserLites);
+
+router.get("/songs/:playlistId", isAuth, refSpotify, getSongsInPlaylist);
+
+router.post("/widget", isAuth, updateWidgetPrivacy);
+
+router.post("/playlist/addSong", isAuth, refSpotify, addSong);
 
 // router.get("/build", isAuth, refSpotify, buildProfile);
 
